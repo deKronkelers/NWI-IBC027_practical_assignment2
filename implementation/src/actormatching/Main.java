052ae613fe949aa28c6b7269e4ccfefbdbaea215
Hendrikto <hendrik.to@gmail.com>
2016-12-18 19:22:35 +0100

    Add a main class.

---
+++ implementation/src/actormatching/Main.java
@@ -0,0 +1,11 @@
+package actormatching;
+
+/**
+ * @author Hendrik Werner // s4549775
+ * @author Constantin Blach // s4329872
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
