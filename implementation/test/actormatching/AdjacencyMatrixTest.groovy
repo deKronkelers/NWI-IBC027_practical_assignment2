06ac80b00b2757ea6ca780106aee2e1b568c5c2a
Hendrikto <hendrik.to@gmail.com>
2016-12-26 20:26:45 +0100

    Test setting things adjacent.

--- implementation/test/actormatching/AdjacencyMatrixTest.groovy
+++ implementation/test/actormatching/AdjacencyMatrixTest.groovy
@@ -4,4 +4,13 @@ package actormatching
  * @author Hendrik Werner // s4549775
  */
 class AdjacencyMatrixTest extends GroovyTestCase {
+    void testSetAdjacent() {
+        AdjacencyMatrix graph = new AdjacencyMatrix(1)
+        assert graph.matrix[0][0] == 0
+        graph.setAdjacent(0, 0)
+        assert graph.matrix[0][0] == 1
+        // test if parallel edges are disallowed
+        graph.setAdjacent(0, 0)
+        assert graph.matrix[0][0] == 1
+    }
 }
