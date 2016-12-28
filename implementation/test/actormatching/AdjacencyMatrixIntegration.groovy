bcfa89248d4225bb65fe812f179a8adfef2f68b7
Hendrikto <hendrik.to@gmail.com>
2016-12-28 15:34:17 +0100

    Test reading matrices from a stream and printing them out.

--- implementation/test/actormatching/AdjacencyMatrixIntegration.groovy
+++ implementation/test/actormatching/AdjacencyMatrixIntegration.groovy
@@ -4,4 +4,10 @@ package actormatching
  * @author Hendrik Werner // s4549775
  */
 class AdjacencyMatrixIntegration extends GroovyTestCase {
+    void testReadingAndPrinting() {
+        InputStream stream = new FileInputStream("../samples/a1.in")
+        assert new AdjacencyMatrix(stream).toString() == "[1, 1]\n[1, 0]"
+        stream = new FileInputStream("../samples/a2.in")
+        assert new AdjacencyMatrix(stream).toString() == "[1, 1]\n[0, 0]"
+    }
 }
