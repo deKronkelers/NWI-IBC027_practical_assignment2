e30a029e306e52a0e26f06dde6a791718197ec09
Hendrikto <hendrik.to@gmail.com>
2016-12-28 16:48:22 +0100

    Add a static utility method to load the test cases.

---
+++ implementation/test/actormatching/TestUtils.groovy
@@ -0,0 +1,18 @@
+package actormatching
+
+/**
+ * @author Hendrik Werner // s4549775
+ */
+class TestUtils {
+    static Map<String, String> loadTestCases(File directory) {
+        Map<String, String> testCases = [] as HashMap
+        File[] inputs = directory.listFiles({
+            File dir, String name -> name.endsWith ".in"
+        } as FilenameFilter)
+        for (File input in inputs) {
+            String testOutput = (input.path.replace(".in", ".out") as File).text
+            testCases.put input.text, testOutput.trim()
+        }
+        testCases
+    }
+}
